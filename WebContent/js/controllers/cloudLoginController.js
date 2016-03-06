0ba17f83631f0b31632aadb406d400dc6808d439
xmiolo <ggrbedin@gmail.com>
2016-03-06 19:13:30 -0300

    Criando User e Logando apos a criação. Projeto ja estruturado

--- WebContent/js/controllers/cloudLoginController.js
+++ WebContent/js/controllers/cloudLoginController.js
@@ -1,9 +1,9 @@
 angular.module("cloudLogin").controller(
 		"cloudLoginController",
-		function($scope, $http, md5) {
-
-			$scope.pass;
-			$scope.usr;
+		function($scope, $http, md5, $location, $window) {
+			$scope.newUser = [];
+			$scope.Lpass;
+			$scope.Lusr;
 			var consultaWSLogin = function() {
 				$http.get("http://localhost:8080/Cloud/service/auth/5")
 						.success(function(data, status) {
@@ -11,20 +11,38 @@ angular.module("cloudLogin").controller(
 						})
 			};
 			consultaWSLogin();
-
-			$scope.fazerPost = function(usr, passEncr) {
+			
+			$scope.redirect = function(){
+				$window.location.href="C:/Users/Gregori/git/AngularJS/WebContent/restrict/welcome.html";
+				
+			};
+			
+			$scope.fazerPost = function(Lusr, LpassEncr) {
 				console.log("entrei");
 				$http.post("http://localhost:8080/Cloud/service/auth/post",
 						JSON.stringify({
-							"name" : usr,
-							"password" : md5.createHash(passEncr)
+							"mail" : Lusr,
+							"pass" : md5.createHash(LpassEncr)
 						})).success(function(data, status, headers, config) {
-					// this callback will be called asynchronously
-					// when the response is available
-					console.log(data);
+					//Quando da SUcess no Login
+					$scope.redirect();
 				}).error(function(data, status, headers, config) {
-					// called asynchronously if an error occurs
-					// or server returns response with an error status.
+					console.log("NAO LOGOU");
+				});
+			}
+			
+			$scope.createNewUser = function(newUser) {
+				console.log("entrei");
+				$http.post("http://localhost:8080/Cloud/service/user/new",
+						JSON.stringify({
+							"name" : newUser.name,
+							"pass" : md5.createHash(newUser.pass),
+							"mail" : newUser.mail
+						})).success(function(data, status, headers, config) {
+					console.log("DEU BOM");
+					$scope.redirect();
+				}).error(function(data, status, headers, config) {
+					console.log("DEU RUIM");
 				});
 			}
 		});
