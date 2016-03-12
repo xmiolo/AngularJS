b12c2e1f3b0efa2f654f0631dd41da796eb690b2
xmiolo <ggrbedin@gmail.com>
2016-03-11 23:09:55 -0300

    Agora sim,alert

--- WebContent/js/controllers/uploadController.js
+++ WebContent/js/controllers/uploadController.js
@@ -6,16 +6,16 @@ angular.module("uploadFiles").controller("uploadController", function($scope) {
 	 */
 	$scope.uploadme = {};
 	$scope.uploadme.src = "";
-
 	$scope.fazerPost = function() {
 		$scope.fileT = document.getElementById("myfiles");
+		
 		$scope.file = $scope.fileT.files;
-		var fl = files.length;
+		var fl = $scope.file.length;
 		var i = 0
 		while (i < fl) {
 			// localize file var in the loop
-			$scope.file = $scope.fileT[i];
-			alert($scope.file.name);
+			$scope.filePrint = $scope.file[i];
+			alert($scope.filePrint.name);
 			i++;
 		}
 	}
